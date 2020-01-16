c0189acf865b79f3d036d714198cffc6c3d920cf
Tyler <tyler@trasmussen.me>
2020-01-16 03:48:04 -0500

    Initial setup. Created PackageInstaller class with basic input parsing capabilities.

---
+++ PackageInstaller/PackageInstaller.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Installer
+{
+    public class PackageInstaller
+    {
+        /*
+         * Main method to run and test PackageInstaller class
+         */
+        static void Main(string[] args)
+        {
+            string[] inputArr = { "A: B", "B: ", "C: A" };
+
+            PackageInstaller PI = new PackageInstaller(inputArr);
+
+            PI.PrintPkgList();
+        }
+
+
+
+
+
+        /*
+         * Actual class def starts here
+         */
+
+        List<string[]> pkgList;
+
+        public PackageInstaller(string[] packages)
+        {
+            pkgList = new List<string[]>();
+
+            foreach (string s in packages)
+            {
+                pkgList.Add(s.Split(": "));
+            }
+        }
+
+        public void SupplyNewPackages(string[] packages)
+        {
+            pkgList = new List<string[]>();
+
+            foreach (string s in packages)
+            {
+                pkgList.Add(s.Split(": "));
+            }
+        }
+
+        public void PrintPkgList()
+        {
+            foreach (string[] s in pkgList)
+            {
+                Console.WriteLine(s[0] + " | " + s[1]);
+            }
+        }
+
+    }
+}
